06cf44c57cf352f09e3423663da56c7ab3715112
jmg2107 <jenjengoo@gmail.com>
2016-08-05 16:10:16 -0700

    (feature) Add Express server, delete Loopback Setup initial route to static html page

--- server/server.js
+++ server/server.js
@@ -1,27 +1,9 @@
-var loopback = require('loopback');
-var boot = require('loopback-boot');
+var express = require('express');
+//var bodyParser = require('body-parser');
+var app = express();
 
-var app = module.exports = loopback();
+require('./middleware.js')(app, express);
+require('./routes.js')(app, express);
 
-app.start = function() {
-  // start the web server
-  return app.listen(function() {
-    app.emit('started');
-    var baseUrl = app.get('url').replace(/\/$/, '');
-    console.log('Web server listening at: %s', baseUrl);
-    if (app.get('loopback-component-explorer')) {
-      var explorerPath = app.get('loopback-component-explorer').mountPath;
-      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
-    }
-  });
-};
 
-// Bootstrap the application, configure models, datasources and middleware.
-// Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
-  if (err) throw err;
-
-  // start the server if `$ node server.js`
-  if (require.main === module)
-    app.start();
-});
+module.exports = app;
