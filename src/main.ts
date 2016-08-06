9a984c75d7216d47d45171c3ab2a73d1a2729c73
jmg2107 <jenjengoo@gmail.com>
2016-08-06 13:20:37 -0700

    (fix) Add typings directory Resolve rxjs versioning problem

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 
-import { AppComponent } from '.app/app.component';
+import { AppComponent } from './app/app.component';
 
 bootstrap(AppComponent, []);
--- src/vendor.ts
+++ src/vendor.ts
@@ -11,5 +11,4 @@ import '@angular/router';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
-import 'rxjs/add/observable/throw';
 import 'rxjs/Rx';
