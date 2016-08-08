3a4a8733312e8762c3cb2c1d54047b7178290953
jmg2107 <jenjengoo@gmail.com>
2016-08-08 14:44:14 -0700

    (feature) Add route to create Smitten Calendar to Google Calendar Integrate Google Calendar API

--- server/requestHandler.js
+++ server/requestHandler.js
@@ -1,14 +1,17 @@
 var google = require('googleapis');
 var OAuth2 = google.auth.OAuth2;
 var client = require('./client_secret.json');
+var request = require('request');
 
 var oauth2Client = new OAuth2(client.web.client_id, client.web.client_secret, client.web.redirect_uris[1]);
+var calendar = google.calendar('v3');
 
 // generate a url that asks permissions for Google+ and Google Calendar scopes
 var scopes = [
   'https://www.googleapis.com/auth/calendar'
 ];
 
+var calendarUrl = "https://www.googleapis.com/calendar/v3/calendars";
 
 module.exports = {
 
@@ -27,6 +30,7 @@ module.exports = {
   googleRedirect: function(req, res, next){
     //use req.params.code
     console.log('code ', req.query.code);
+
     oauth2Client.getToken(req.query.code, function(err, tokens) {
     // Now tokens contains an access_token and an optional refresh_token. Save them.
       if(!err) {
@@ -36,6 +40,25 @@ module.exports = {
 
     });
     res.redirect('/');
+  },
+
+  calendarCreate: function(req, res, next){
+
+    calendar.calendars.insert({
+      auth: oauth2Client,
+      resource: {
+      summary: 'Smitten'
+      }
+    }, function(err, event){
+      if(err){
+        console.log("calendar error: ", err);
+      }
+      console.log("Smitten calendar created ", event);
+    });
+
   }
+
+
+
 
 };
