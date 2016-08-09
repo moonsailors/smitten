13d733bcfc6427abd362ae63daa2c18239081312
Heather Park <hthr.prk@gmail.com>
2016-08-09 14:00:36 -0700

    Resolve merge conflicts

--- src/app/services/apiService.ts
+++ src/app/services/apiService.ts
@@ -13,16 +13,16 @@ export class ApiService {
 
   api_url: string = 'http://localhost:3000';
 
-  constructor(private http: Http){}
+  constructor(private http: Http) {}
 
-  private getJson(response: Response){
+  private getJson(response: Response) {
     return response.json();
   }
 
-  private checkForError(response: Response): Response{
-    if(response.status >= 200  && response.status < 300){
+  private checkForError(response: Response): Response {
+    if (response.status >= 200  && response.status < 300) {
       return response;
-    }else{
+    }else {
       var error = new Error(response.statusText);
       error['response'] = response;
       console.error(error);
@@ -30,24 +30,24 @@ export class ApiService {
     }
   }
 
-  get(path: string): Observable<any>{
+  get(path: string): Observable<any> {
     return this.http.get(`${this.api_url}${path}`, {headers: this.headers})
       .map(this.checkForError)
-      .catch(err =>Observable.throw(err))
+      .catch(err => Observable.throw(err))
       .map(this.getJson);
   }
 
-  post(path: string, body): Observable<any>{
+  post(path: string, body): Observable<any> {
     return this.http.post(`${this.api_url}${path}`, JSON.stringify(body), {headers: this.headers})
      .map(this.checkForError)
-     .catch(err =>Observable.throw(err))
+     .catch(err => Observable.throw(err))
      .map(this.getJson);
   }
 
-  delete(path: string): Observable<any>{
+  delete(path: string): Observable<any> {
     return this.http.delete(`${this.api_url}${path}`, {headers: this.headers})
       .map(this.checkForError)
-      .catch(err =>Observable.throw(err))
+      .catch(err => Observable.throw(err))
       .map(this.getJson);
   }
 
