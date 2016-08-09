18aad7731f15d1194b6588b91bc21803f5d69b35
Heather Park <hthr.prk@gmail.com>
2016-08-08 17:13:18 -0700

    (feat) Update server-side routing to include POST requests for Amazon search

--- server/routes.js
+++ server/routes.js
@@ -3,9 +3,7 @@ var requestHandler = require('./requestHandler.js');
 module.exports = function (app, express){
 
   app.get('/api/googleLogin', requestHandler.googleLogin);
-
   app.get('/googleOAuth/', requestHandler.googleRedirect);
-
   app.get('/api/calendarCreate', requestHandler.calendarCreate);
 
   app.post('/api/amazonSearch', requestHandler.amazonSearchItem);
