8cf6deaae424eea4b123a730280fb8ab88420190
Heather Park <hthr.prk@gmail.com>
2016-08-09 12:52:15 -0700

    Update middleware file to serve static files from 'build' directory

--- server/middleware.js
+++ server/middleware.js
@@ -4,6 +4,6 @@ var bodyParser = require('body-parser');
 module.exports = function (app, express){
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
-  app.use(express.static('src'));
+  app.use(express.static('build'));
 
 };
