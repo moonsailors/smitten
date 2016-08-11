9e11e77d81bab92e708c4e5aa5fd41b70fce0e37
Karen Zapata <kzapata0919@gmail.com>
2016-08-11 10:30:51 -0700

    (feat) Add Login styling

--- src/app/ui/index.ts
+++ src/app/ui/index.ts
@@ -4,4 +4,3 @@ export { WishInput } from './wishInput';
 export { LoginInput } from './login';
 export { AmazonSearchResultsComponent } from './amazon-search-results.component';
 export { AmazonSearchInputComponent } from './amazon-search-input.component';
-
