462d89e9f008bfcd0fa3e3a513160af572f9763c
Karen Zapata <kzapata0919@gmail.com>
2016-08-11 10:38:19 -0700

    (resolve) Resolve merge conflict in index.ts

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,15 +1,9 @@
 export { App } from './app';
 export { routes } from './routes';
 export { WishService, CalendarService, ApiService } from './services/index';
-<<<<<<< 24b94010e975f4a32228763f0bf014d4adeebb72
 export { Wishes, Main, Calendar, Login } from './containers/index';
 export { WishCard, CalendarInput } from './ui/index';
 
-=======
-export { Wishes, Main } from './containers/index';
-export { WishCard, Calendar } from './ui/index';
-export { Login } from './ui/index';
->>>>>>> (feat) Add Login styling
 // import * as services from './services';
 // console.log("services are ", services);
 // const mapValuesToArray = (obj) => Object.keys(obj).map(key => obj[key]);
