b3527ff76c4b61ac5888669eec0fc62a07b4ccf9
Heather Park <hthr.prk@gmail.com>
2016-08-10 22:42:42 -0700

    (feat) Export AmazonSearchComponent as module in containers/index.ts

---
+++ src/app/ui/amazon-search-results.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'amazon-search-results',
+  styles: [],
+  template: `
+
+  `
+})
+
+export class AmazonSearchResultsComponent {}
