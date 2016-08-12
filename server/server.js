62d725733bccfad460d761b32263b5ee6780168d
Heather Park <hthr.prk@gmail.com>
2016-08-12 13:24:30 -0700

    (feat) Set up socket.io on server side

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 var express = require('express');
-//var bodyParser = require('body-parser');
 var app = express();
+// 'app' is a function handler to be used in the HTTP server created below
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 require('./middleware.js')(app, express);
 require('./routes.js')(app, express);
 
+io.on('connection', function(socket) {
+  console.log('a user connected');
+});
 
-module.exports = app;
+// export 'server' instead of 'app' to have it listen for a port in index.js
+module.exports = server;
