c9fa0fb6561a5c8abdbd9104311ef1baac84f0bb
Heather Park <hthr.prk@gmail.com>
2016-08-17 16:46:01 -0700

    (feat) Add event emitter

--- src/app/mixtape/search/search-input.component.ts
+++ src/app/mixtape/search/search-input.component.ts
@@ -1,27 +1,23 @@
-import { Component } from '@angular/core';
-
-import { SearchSoundCloud } from './search.service';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'sc-search-input',
-  templateUrl: 'app/mixtape/search/search-input.component.html',
-  providers: [SearchSoundCloud]
+  templateUrl: 'app/mixtape/search/search-input.component.html'
 })
 export class SoundCloudSearchInputComponent {
+  @Output() onSearchSubmit = new EventEmitter<any>();
+
   resultMax: number = 50;
   search = {
     q: '',
     limit: this.resultMax
   };
-  searchResults: Array<any>;
-
-  constructor(private searchSoundCloud: SearchSoundCloud) {}
 
   onSubmit() {
     console.log('sending your SoundCloud search query to server');
 
     if (this.search.q) {
-      this.searchSoundCloud.search(this.search);
+      this.onSearchSubmit.emit(this.search);
     }
 
     this.search.q = '';
