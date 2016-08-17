57c62a4bc0413c3b7d62d8622bba4758b2a2d574
Heather Park <hthr.prk@gmail.com>
2016-08-17 12:58:48 -0700

    (chore) Disable socket.io to prevent browsersync interference

--- server/server.js
+++ server/server.js
@@ -8,9 +8,9 @@ require('./middleware.js')(app, express);
 require('./routes.js')(app, express);
 
 // listen for 'connection' event for incoming sockets
-io.on('connection', function(socket) {
-  console.log('a user connected');
-});
+// io.on('connection', function(socket) {
+//   console.log('a user connected');
+// });
 
 // export 'server' instead of 'app' to have it listen for a port in index.js
 module.exports = server;
