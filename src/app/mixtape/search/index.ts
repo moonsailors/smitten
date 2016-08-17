5ec68f3e01e3c62b7fc7e2f50453164397a0ed4b
Heather Park <hthr.prk@gmail.com>
2016-08-16 17:14:36 -0700

    (feat) Create index page for mixtape's search directory

---
+++ src/app/mixtape/search/index.ts
@@ -0,0 +1,3 @@
+import { SoundCloudSearchComponent } from './search.component';
+import { SoundCloudSearchInputComponent } from './search-input.component';
+import { SoundCloudSearchResultsComponent } from './search-results.component';
