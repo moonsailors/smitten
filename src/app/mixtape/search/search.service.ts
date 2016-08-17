fc95fa1e2b0b524b54984588f33e6c7f10b0670a
Heather Park <hthr.prk@gmail.com>
2016-08-17 16:48:07 -0700

    (feat) Change service logic so it only makes HTTP request

--- src/app/mixtape/search/search.service.ts
+++ src/app/mixtape/search/search.service.ts
@@ -1,25 +1,14 @@
 import { Injectable } from '@angular/core';
-import {Observable} from 'rxjs/Observable';
-import 'rxjs/Rx';
 
 import { ApiService } from '../../services/index';
 
 @Injectable()
 export class SearchSoundCloud {
-  results: Observable<any>;
-
   constructor(private apiService: ApiService) {}
 
   search(searchParams) {
     console.log('making the API call to SoundCloud');
 
-    this.apiService.post('/api/soundcloud-search', searchParams)
-      .subscribe(
-        results => {
-          console.log('soundcloud results received from server');
-          this.results = results._body;
-        },
-        err => console.log('error: ', err),
-        () => console.log(this.results));
+    return this.apiService.post('/api/soundcloud-search', searchParams);
   }
 }
