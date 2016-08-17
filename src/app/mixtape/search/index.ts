ce0455fb030f5e2b5d0a8a4699679cc5c7e1d726
Heather Park <hthr.prk@gmail.com>
2016-08-16 17:47:24 -0700

    (chore) Fix linting errors

--- src/app/mixtape/search/index.ts
+++ src/app/mixtape/search/index.ts
@@ -1,3 +1,7 @@
-import { SoundCloudSearchComponent } from './search.component';
-import { SoundCloudSearchInputComponent } from './search-input.component';
-import { SoundCloudSearchResultsComponent } from './search-results.component';
+/** components **/
+export { SoundCloudSearchComponent } from './search.component';
+export { SoundCloudSearchInputComponent } from './search-input.component';
+export { SoundCloudSearchResultsComponent } from './search-results.component';
+
+/** services **/
+export { SearchSoundCloud } from './search.service';
