fe042319eab2cbb2fd716539ace194f022531e7b
Heather Park <hthr.prk@gmail.com>
2016-08-17 15:04:11 -0700

    (chore) Fix linting and routing errors

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,7 +3,7 @@ import { Main,
         Wishes,
         Calendar,
         Login } from './containers/index';
-import { SoundCloudSearchComponent } from './mixtape/search/index';
+import { SoundCloudSearchComponent } from './mixtape/search/search.component';
 
 
 export const routes: RouterConfig = [
