a1def9a2f9cd0e1912fc44814987db237bd38eaf
Heather Park <hthr.prk@gmail.com>
2016-08-20 11:32:20 -0700

    (chore) Remove service injection from SoundCloudSearchInputComponent

--- src/app/mixtape/search/search-input.component.ts
+++ src/app/mixtape/search/search-input.component.ts
@@ -1,24 +1,38 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 
+import { SearchSoundCloud } from './search.service';
+import { Store } from '../../store/store';
+
 @Component({
   selector: 'sc-search-input',
   templateUrl: 'app/mixtape/search/search-input.component.html',
-  styleUrls: ['app/mixtape/search/search-input.component.css']
+  styleUrls: ['app/mixtape/search/search-input.component.css'],
+  providers: [SearchSoundCloud]
 })
 export class SoundCloudSearchInputComponent {
-  @Output() onSearchSubmit = new EventEmitter<any>();
-
   resultMax: number = 1;
   search = {
     q: '',
     limit: this.resultMax
   };
 
-  onSubmit() {
-    if (this.search.q) {
-      this.onSearchSubmit.emit(this.search);
-    }
+  constructor(private store: Store,
+              private searchSoundCloud: SearchSoundCloud) {}
 
+  onSubmit(searchParams) {
+    console.log('search submitted');
+    const currentState = this.store.getState();
+
+    if (this.search.q) {
+      this.searchSoundCloud.search(searchParams)
+        .subscribe(
+          results => {
+            currentState.mixtape.playlist = results;
+          },
+          err => console.log('error: ', err),
+          () => this.store.setState(currentState));
+
+    }
     this.search.q = '';
   }
 }
