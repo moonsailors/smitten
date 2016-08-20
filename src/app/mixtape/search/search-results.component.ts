eefa41c04d5ca6edb1402cdbec6c8d6f530b56e4
Heather Park <hthr.prk@gmail.com>
2016-08-20 16:30:14 -0700

    (feat) Have mixtape search results component update store correctly

--- src/app/mixtape/search/search-results.component.ts
+++ src/app/mixtape/search/search-results.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/Rx';
 
+import { PlaylistService } from '../shared/playlist.service';
 import { Store } from '../../store/store';
 
 @Component({
   selector: 'sc-search-results',
   templateUrl: 'app/mixtape/search/search-results.component.html',
-  styleUrls: ['app/mixtape/search/search-results.component.css']
+  styleUrls: ['app/mixtape/search/search-results.component.css'],
+  providers: [PlaylistService]
 })
 export class SoundCloudSearchResultsComponent {
   searchResults: Observable<Array<Object>>;
 
-  constructor(private store: Store) {
+  constructor(private store: Store,
+              private playlistService: PlaylistService) {
     this.store
       .changes
       .pluck('mixtape', 'searchResults')
@@ -21,5 +24,27 @@ export class SoundCloudSearchResultsComponent {
           this.searchResults = searchResults,
         err => console.log('error: ', err),
         () => console.log(this.searchResults));
+  }
+
+  addSong(songData) {
+    const currentState = this.store.getState();
+
+    this.playlistService.addToPlaylist(songData)
+      .subscribe(
+        song => {
+          song = [JSON.parse(song._body)];
+          this.store.setState(
+            Object.assign({}, currentState, {
+              mixtape: {
+                playlist: [
+                  ...currentState.mixtape.playlist,
+                  ...song
+                ],
+                searchResults: currentState.mixtape.searchResults
+              }
+            })
+          );
+        },
+        err => console.log('error: ', err));
   }
 }
