6dc90556bf7a9004d4af4f19a8f1d568df4ed266
Heather Park <hthr.prk@gmail.com>
2016-08-20 16:28:30 -0700

    (feat) Update PlaylistComponent to subscribe to changes in store

--- src/app/mixtape/playlist/playlist.component.ts
+++ src/app/mixtape/playlist/playlist.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
-import { PlaylistService } from '../shared/index';
+import { PlaylistService } from '../shared/playlist.service';
+import { Store } from '../../store/store';
 
 @Component({
   selector: 'playlist',
-  templateUrl: 'app/mixtape/playlist/playlist.component.html',
-  providers: [PlaylistService]
+  templateUrl: 'app/mixtape/playlist/playlist.component.html'
 })
-export class PlaylistComponent {}
+export class PlaylistComponent implements OnInit {
+  songs: Observable<Array<Object>>;
+
+  ngOnInit() {
+
+  }
+
+  // TODO: refactor code
+  constructor(private store: Store) {
+    this.store
+      .changes
+      .pluck('mixtape', 'playlist')
+      .subscribe(
+        (playlist: Observable<Array<Object>>) =>
+          this.songs = playlist,
+        err => console.log('error: ', err));
+  }
+}
