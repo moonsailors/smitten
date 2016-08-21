1e1c3116c5a6eb0b2673777371528e40388b05ec
Heather Park <hthr.prk@gmail.com>
2016-08-21 14:08:28 -0700

    (feat) Update component to handle clicking song in playlist

--- src/app/mixtape/player/player.component.ts
+++ src/app/mixtape/player/player.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/Rx';
 
 import { MixtapePlayerService } from './player.service';
 import { Song } from '../shared/index';
@@ -26,17 +25,53 @@ export class MixtapePlayerComponent implements OnInit {
       .subscribe(
         (playlist: Observable<Array<Object>>) => {
           this.playerService.playlist = Array.prototype.slice.call(playlist);
+
           if (!this.nowPlaying && this.playerService.playlist.length) {
             this.nowPlaying = this.playerService.playlist[0];
             this.src = this.nowPlaying.stream + '?client_id=' + this.clientId;
           }
         },
         err => console.log('error: ', err));
+
+     this.store
+      .changes
+      .pluck('mixtape', 'nowPlaying')
+      .subscribe(
+        (song: Song) => {
+          if (song) {
+            this.paused = true;
+            this.setSong(song);
+            this.togglePlay();
+          }
+        },
+        err => console.log('error: ', err)
+      );
   }
 
   ngOnInit() {
     this.audio = document.getElementById('mixtape-audio');
+    this.audio.addEventListener('ended', this.playNextSong);
     this.clientId = '370cba66667bcfda9e137b49ec27b708';
+    this.paused = true;
     this.playerService.currentSongIndex = 0;
+  }
+
+  playNextSong() {
+
+  }
+
+  setSong(songData) {
+    this.nowPlaying = songData;
+    this.src = this.nowPlaying.stream + '?client_id=' + this.clientId;
+  }
+
+  togglePlay() {
+    if (this.paused) {
+      this.audio.play();
+      this.paused = false;
+    } else {
+      this.audio.pause();
+      this.paused = true;
+    }
   }
 }
