4bc563306d4a662cfb247e9f4eae0bda9c44dc38
Heather Park <hthr.prk@gmail.com>
2016-08-20 17:48:12 -0700

    (feat) Update PlaylistComponent to fetch playlist from server on init

--- src/app/mixtape/playlist/playlist.component.ts
+++ src/app/mixtape/playlist/playlist.component.ts
@@ -7,23 +7,44 @@ import { Store } from '../../store/store';
 
 @Component({
   selector: 'playlist',
-  templateUrl: 'app/mixtape/playlist/playlist.component.html'
+  templateUrl: 'app/mixtape/playlist/playlist.component.html',
+  providers: [PlaylistService]
 })
 export class PlaylistComponent implements OnInit {
   songs: Observable<Array<Object>>;
 
-  ngOnInit() {
-
-  }
-
   // TODO: refactor code
-  constructor(private store: Store) {
+  constructor(private store: Store,
+              private playlistService: PlaylistService) {
     this.store
       .changes
       .pluck('mixtape', 'playlist')
       .subscribe(
-        (playlist: Observable<Array<Object>>) =>
-          this.songs = playlist,
+        (playlist: Observable<Array<Object>>) => {
+          this.songs = playlist;
+          console.log('our playlist: ', this.songs);
+        },
+        err => console.log('error: ', err));
+  }
+
+  ngOnInit() {
+    const currentState = this.store.getState();
+
+    this.playlistService.getPlaylist()
+      .subscribe(
+        playlist => {
+          playlist = JSON.parse(playlist._body);
+
+          this.store.setState(
+            Object.assign({}, currentState, {
+              mixtape: {
+                playlist: playlist,
+                searchResults: currentState.mixtape.searchResults
+              }
+            })
+          );
+
+        },
         err => console.log('error: ', err));
   }
 }
