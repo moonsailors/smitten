c4cce4a74198243330f1764c8bad74cd81ac00fd
Heather Park <hthr.prk@gmail.com>
2016-08-22 20:28:54 -0700

    (feat) Create necessary data for nav bar

---
+++ src/app/landing/nav/nav-brand.ts
@@ -0,0 +1,4 @@
+export const NAV_BRAND: Object = {
+  text: 'Smitten',
+  url: 'http://just-smitten.herokuapp.com'
+};
---
+++ src/app/landing/nav/nav-links.ts
@@ -0,0 +1,6 @@
+export const NAV_LINKS: Object[] = [
+  {text: 'Features', url: '#landing-features'},
+  {text: 'Testimonials', url: '#landing-testimonials'},
+  {text: 'Team', url: '#landing-team'},
+  {text: 'Log In', url: '#'}
+];
