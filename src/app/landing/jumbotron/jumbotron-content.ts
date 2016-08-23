861202a991b8e04f1b97e92aacfb2f78ac79a578
Heather Park <hthr.prk@gmail.com>
2016-08-22 23:40:14 -0700

    (feat) Create files for jumbotron

---
+++ src/app/landing/jumbotron/jumbotron-content.ts
@@ -0,0 +1,8 @@
+export const JUMBOTRON_CONTENT: Object = {
+  title: 'Smitten',
+  tagline: 'Streamlining communication between you and that special someone',
+  button: {
+    text: 'Learn More',
+    url: '#landing-features'
+  }
+};
---
+++ src/app/landing/jumbotron/jumbotron.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { JumbotronService } from './jumbotron.service';
+
+@Component({
+  selector: 'jumbotron',
+  templateUrl: 'app/landing/jumbotron/jumbotron.component.html',
+  styleUrls: ['app/landing/jumbotron/jumbotron.component.css'],
+  providers: [JumbotronService]
+})
+export class JumbotronComponent implements OnInit {
+  content: Object;
+
+  constructor(private jumbotronService: JumbotronService) {}
+
+  getContent() {
+    this.jumbotronService.getContent()
+      .then(content => this.content = content)
+      .then(() => console.log('content: ', this.content));
+  }
+
+  ngOnInit() {
+    this.getContent();
+  }
+}
