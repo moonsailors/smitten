103a32c21d16b9a2f7a2a82b177353f3a43d2ec1
Heather Park <hthr.prk@gmail.com>
2016-08-23 10:08:52 -0700

    (feat) Create content for landing page testimonials section Update testimonials service to export correctly

---
+++ src/app/landing/testimonials/testimonials-content.ts
@@ -0,0 +1,15 @@
+export const TESTIMONIALS_CONTENT: Object = {
+  heading: 'Testimonials',
+  testimonials: [
+    {
+      user: 'Kim Kardashian',
+      testimonial: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+    },
+    {
+      user: 'Kanye West',
+      testimonial: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+    }
+  ]
+};
--- src/app/landing/testimonials/testimonials.service.ts
+++ src/app/landing/testimonials/testimonials.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { TESTIMONIALS_CONTENT } from './testimonials-content';
 
 @Injectable()
-export class FeaturesService {
+export class TestimonialsService {
   getContent(): Promise<Object[]> {
     return Promise.resolve(TESTIMONIALS_CONTENT);
   }
