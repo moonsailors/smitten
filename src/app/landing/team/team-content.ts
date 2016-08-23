392fbabc6dfc809a0b2d45e5ccc546dc3cb54b60
Heather Park <hthr.prk@gmail.com>
2016-08-23 11:22:38 -0700

    (feat) Add content for landing page team section

---
+++ src/app/landing/team/team-content.ts
@@ -0,0 +1,22 @@
+export const TEAM_CONTENT: Object {
+  heading: 'The Team',
+  team: [
+    {
+      name: 'Connor Campbell',
+      role: 'Product Owner'
+    },
+    {
+      name: 'Heather Park',
+      role: 'Scrum Master'
+    },
+    {
+      name: 'Jennica Goo',
+      role: 'Developer'
+    },
+    {
+      name: 'Karen Zapata',
+      role: 'Developer'
+    }
+  ],
+  githubImg: 'app/landing/team/images/github-icon.png'
+};
