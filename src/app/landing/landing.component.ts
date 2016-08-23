397b262a2167beb60acbe6f603411b55d6613551
Heather Park <hthr.prk@gmail.com>
2016-08-22 23:38:56 -0700

    (feat) Inject jumbotron component into landing page component

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
+import { JumbotronComponent } from './jumbotron/index';
 import { LandingNavComponent } from './nav/index';
 
 @Component({
   selector: 'landing',
-  directives: [LandingNavComponent],
+  directives: [JumbotronComponent, LandingNavComponent],
   template: `
     <landing-nav></landing-nav>
+    <jumbotron></jumbotron>
   `
 })
 export class LandingPageComponent {}
