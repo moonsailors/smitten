282c48b84b8a8d36ce2d2592b2e2774223cbd7f1
Heather Park <hthr.prk@gmail.com>
2016-08-23 00:07:15 -0700

    (feat) Create content object for landing page features component

---
+++ src/app/landing/features/features-content.ts
@@ -0,0 +1,21 @@
+export const FEATURES_CONTENT: Object = {
+  heading: 'Features',
+  features: [
+    {
+      name: 'Mood Analysis',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor.',
+    },
+    {
+      name: 'Shared Calendar',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor.',
+    },
+    {
+      name: 'Love Board',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor.',
+    },
+    {
+      name: 'Digital Mixtape',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor.',
+    }
+  ]
+};
