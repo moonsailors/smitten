c0ac5236ae952513b5f8dc0c80511fc74444c791
Heather Park <hthr.prk@gmail.com>
2016-08-23 00:00:59 -0700

    (feat) Inject features component as directive to landing component

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 
+import { FeaturesComponent } from './features/index';
 import { JumbotronComponent } from './jumbotron/index';
 import { LandingNavComponent } from './nav/index';
 
 @Component({
   selector: 'landing',
-  directives: [JumbotronComponent, LandingNavComponent],
+  directives: [
+    FeaturesComponent,
+    JumbotronComponent,
+    LandingNavComponent
+  ],
   template: `
     <landing-nav></landing-nav>
     <jumbotron></jumbotron>
+    <features></features>
   `
 })
 export class LandingPageComponent {}
