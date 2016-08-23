14d86a98989dabbd107dcf0d2911a34a03e6da6a
Heather Park <hthr.prk@gmail.com>
2016-08-22 21:44:23 -0700

    (feat) Style landing page nav bar

--- src/app/landing/nav/nav-links.ts
+++ src/app/landing/nav/nav-links.ts
@@ -1,6 +1,5 @@
 export const NAV_LINKS: Object[] = [
   {text: 'Features', url: '#landing-features'},
   {text: 'Testimonials', url: '#landing-testimonials'},
-  {text: 'Team', url: '#landing-team'},
-  {text: 'Log In', url: '#'}
+  {text: 'Team', url: '#landing-team'}
 ];
