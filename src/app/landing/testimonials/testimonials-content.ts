8b08312f3f6d6a6941f8ab9e8e2f4ecffa54c689
Heather Park <hthr.prk@gmail.com>
2016-08-23 10:13:34 -0700

    (feat) Update testimonials content with image paths

--- src/app/landing/testimonials/testimonials-content.ts
+++ src/app/landing/testimonials/testimonials-content.ts
@@ -4,12 +4,14 @@ export const TESTIMONIALS_CONTENT: Object = {
     {
       user: 'Kim Kardashian',
       testimonial: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+      image: 'app/landing/testimonials/images/smitten-kim.png'
     },
     {
       user: 'Kanye West',
       testimonial: `Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
+      image: 'app/landing/testimonials/images/smitten-kanye.png'
     }
   ]
 };
