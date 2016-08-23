52c057db5dde78008b0f3e7cd4b63884d2338ced
Heather Park <hthr.prk@gmail.com>
2016-08-23 00:54:51 -0700

    (feat) Update landing page features content to include image paths

--- src/app/landing/features/features-content.ts
+++ src/app/landing/features/features-content.ts
@@ -4,18 +4,22 @@ export const FEATURES_CONTENT: Object = {
     {
       name: 'Mood Analysis',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor.',
+      image: 'app/landing/features/images/smitten-graph.png'
     },
     {
       name: 'Shared Calendar',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor.',
+      image: 'app/landing/features/images/smitten-calendar.png'
     },
     {
       name: 'Love Board',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor.',
+      image: 'app/landing/features/images/smitten-heart.png'
     },
     {
       name: 'Digital Mixtape',
       description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit sed do eiusmod tempor.',
+      image: 'app/landing/features/images/smitten-music.png'
     }
   ]
 };
