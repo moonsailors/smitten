4b6b7cf49162265cf296a0b5c154e7608350feff
Heather Park <hthr.prk@gmail.com>
2016-08-23 00:17:07 -0700

    (chore) Remove console log from landing page features component

--- src/app/landing/features/features.component.ts
+++ src/app/landing/features/features.component.ts
@@ -15,8 +15,7 @@ export class FeaturesComponent implements OnInit {
 
   getContent() {
     this.featuresService.getContent()
-      .then(content => this.content = content)
-      .then(() => console.log('content: ', this.content));
+      .then(content => this.content = content);
   }
 
   ngOnInit() {
