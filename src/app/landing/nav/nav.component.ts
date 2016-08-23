17852804d881ff77c8a1bb6ce85e22641f2d6df7
Heather Park <hthr.prk@gmail.com>
2016-08-22 20:28:13 -0700

    (feat) Write necessary functions for LandingNavComponent

--- src/app/landing/nav/nav.component.ts
+++ src/app/landing/nav/nav.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { LandingNavService } from './nav.service';
 
 @Component({
   selector: 'landing-nav',
-  templateUrl: 'app/mixtape/landing/nav/nav.component.html',
-  styleUrls: ['app/mixtape/landing/nav/nav.component.css']
+  templateUrl: 'app/landing/nav/nav.component.html',
+  styleUrls: ['app/landing/nav/nav.component.css'],
+  providers: [LandingNavService]
 })
-export class LandingNavComponent {}
+export class LandingNavComponent implements OnInit {
+  brand: Object;
+  links: Object[];
+
+  constructor(private navService: LandingNavService) {}
+
+  getBrand(): void {
+    this.navService.getBrand()
+      .then(brand => this.brand = brand);
+  }
+
+  getLinks(): void {
+    this.navService.getLinks()
+      .then(links => this.links = links);
+  }
+
+  ngOnInit() {
+    this.getBrand();
+    this.getLinks();
+  }
+}
