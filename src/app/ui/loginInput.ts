7a2ed38dc4b59144107a96fb8a8d2c8685f4c8fd
Heather Park <hthr.prk@gmail.com>
2016-08-23 15:01:45 -0700

    (feat) Add HTML/CSS for login

--- src/app/ui/loginInput.ts
+++ src/app/ui/loginInput.ts
@@ -9,59 +9,57 @@ import { Button, InputText } from 'primeng/primeng';
   selector: 'login-input',
   directives: [Button, InputText],
   styles: [`
-      body {
-        background-image:url(/app/ui/heart-broken.jpeg);
-        background-repeat: no-repeat;
-        background-size: 100% 100%;
-      }
-      .container {
-        margin-left: auto;
-        margin-right: auto;
-      }
-      .login {
-        float:left;
-        margin: 10px;
-        padding: 10px;
-        max-width:300px;
-        height: 300px;
-      }
-      .field {
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        margin: -100px 0 0 -150px;
-      }
-      legend {
-        font-size: 250%;
-      }
-    `
+    .login-page {
+      width: 100%;
+      text-align: center;
+    }
+
+    .login-container {
+      position: absolute;
+      top: 25%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      width: 100%;
+    }
+
+    .color-bar {
+      background-color: #91204D;
+      height: 9px;
+      width: 100%;
+    }
+
+    p {
+      font-size: 1.5625em;
+      font-weight: 700;
+    }
+
+    .login-container img {
+      width: 190px;
+      height: auto;
+      margin-top: 7px;
+    }
+  `
   ],
   template: `
-    <body>
-      <div class="login">
-        <div>
-        <form>
-          <fieldset class="field">
-            <legend>Login to Smitten</legend>
-            <container id="container">
-            <p [hidden]="hitLoginButton || loggedIn" class="submit">
-            <button pButton type="button" (click)="loginUser()"
-            label="Sign in with Google">
-            </button>
-            </p>
-            <p [hidden]="!loggedIn"> Please link your partner
-            <input pInputText type="text" name="login" [(ngModel)]="partner.email" value="" placeholder="Partner Email">
-            </p>
-            <p [hidden]="!loggedIn" class="link-partner">
-            <button pButton type="button" (click)="addPartner()" label="Link partner account">
-            </button>
-            </p>
-            </container>
-          </fieldset>
-        </form>
-        </div>
-      </div>
-    </body>
+  <div class="login-page">
+    <div class="color-bar"></div>
+    <div
+      class="login-container"
+      [hidden]="hitLoginButton || loggedIn">
+      <p>welcome to smitten.</p>
+      <img (click)="loginUser()" src="app/images/google-signin.png">
+    </div>
+    <div
+        class="partner-link-container"
+        [hidden]="!loggedIn">
+        <p>please link your partner.</p>
+        <input
+            type="text"
+            [(ngModel)]="partner.email"
+            placeholder="your partner's gmail address">
+        <button (click)="addPartner()"></button>
+    </div>
+  </div>
   `
 })
 
