f48c113c5d18df6e1482aceb9997c8a8df3e2726
Heather Park <hthr.prk@gmail.com>
2016-08-23 11:47:31 -0700

    (feat) Update team content with individual github links

--- src/app/landing/team/team-content.ts
+++ src/app/landing/team/team-content.ts
@@ -4,23 +4,30 @@ export const TEAM_CONTENT: Object = {
     {
       name: 'Connor Campbell',
       role: 'Product Owner',
-      image: 'app/landing/team/images/smitten-connor.png'
+      image: 'app/landing/team/images/smitten-connor.png',
+      githubImg: 'app/landing/team/images/github-icon.png',
+      githubLink: 'https://github.com/evenkeeler'
     },
     {
       name: 'Heather Park',
       role: 'Scrum Master',
-      image: 'app/landing/team/images/smitten-heather.png'
+      image: 'app/landing/team/images/smitten-heather.png',
+      githubImg: 'app/landing/team/images/github-icon.png',
+      githubLink: 'https://github.com/heatherpark'
     },
     {
       name: 'Jennica Goo',
       role: 'Developer',
-      image: 'app/landing/team/images/smitten-jennica.png'
+      image: 'app/landing/team/images/smitten-jennica.png',
+      githubImg: 'app/landing/team/images/github-icon.png',
+      githubLink: 'https://github.com/jmg2107'
     },
     {
       name: 'Karen Zapata',
       role: 'Developer',
-      image: 'app/landing/team/images/smitten-karen.png'
+      image: 'app/landing/team/images/smitten-karen.png',
+      githubImg: 'app/landing/team/images/github-icon.png',
+      githubLink: 'https://github.com/kzapata19'
     }
   ],
-  githubImg: 'app/landing/team/images/github-icon.png'
 };
