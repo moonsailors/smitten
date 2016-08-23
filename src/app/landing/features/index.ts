d9f25e85a472297a9411a4ed8fe53ce5b9ec20f2
Heather Park <hthr.prk@gmail.com>
2016-08-22 23:59:45 -0700

    (feat) Create directory and index for landing page features section

---
+++ src/app/landing/features/index.ts
@@ -0,0 +1,8 @@
+/** components **/
+export { FeaturesComponent } from './features.component';
+
+/** services **/
+export { FeaturesService } from './features.service';
+
+/** data **/
+export { FEATURES_CONTENT } from './features-content';
