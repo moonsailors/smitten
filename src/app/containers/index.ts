8a4f825cfd7db4fbdf13deda1f361715653e19cb
Karen Zapata <kzapata0919@gmail.com>
2016-08-24 09:52:28 -0700

    (feat) Add neural network and graph component

--- src/app/containers/index.ts
+++ src/app/containers/index.ts
@@ -2,5 +2,4 @@ export { Wishes } from './wishes';
 export { Main } from './main';
 export { Calendar } from './calendar';
 export { Login } from './login';
-export { Logout } from './logout';
-
+export { Graph } from './graph';
