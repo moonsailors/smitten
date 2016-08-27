305d671ab6b8352c21e07f693d409498b2b7ef0b
Heather Park <hthr.prk@gmail.com>
2016-08-26 21:03:00 -0700

    (chore) Rearrange package.json (chore) Remove console logs

--- src/app/index.ts
+++ src/app/index.ts
@@ -5,10 +5,8 @@ export { Wishes, Main, Calendar, Login } from './containers/index';
 export { PostCard, CalendarInput } from './ui/index';
 
 import * as services from './services/index';
-console.log("services are ", services);
 const mapValuesToArray = (obj) => Object.keys(obj).map(key => obj[key]);
 
-console.log("services map ", mapValuesToArray(services));
 export const providers = [
   ...mapValuesToArray(services)
 ];
