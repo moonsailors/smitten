49ace0558002a57239095dede573e3c823365d97
Heather Park <hthr.prk@gmail.com>
2016-08-26 21:05:51 -0700

    (chore) Remove unnecessary code

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,10 +1,5 @@
-export { App } from './app';
-export { routes } from './routes';
-export { Store } from './store';
-export { Wishes, Main, Calendar, Login } from './containers/index';
-export { PostCard, CalendarInput } from './ui/index';
-
 import * as services from './services/index';
+
 const mapValuesToArray = (obj) => Object.keys(obj).map(key => obj[key]);
 
 export const providers = [
