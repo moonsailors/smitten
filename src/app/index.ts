c019955a0b63c78618f3381e3d5e391d9215b65e
Heather Park <hthr.prk@gmail.com>
2016-08-26 21:32:55 -0700

    (chore) Remove console logs from index.ts

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,14 +1,7 @@
-export { App } from './app';
-export { routes } from './routes';
-export { Store } from './store';
-export { Wishes, Main, Calendar, Login } from './containers/index';
-export { PostCard, CalendarInput } from './ui/index';
-
 import * as services from './services/index';
-console.log("services are ", services);
+
 const mapValuesToArray = (obj) => Object.keys(obj).map(key => obj[key]);
 
-console.log("services map ", mapValuesToArray(services));
 export const providers = [
   ...mapValuesToArray(services)
 ];
